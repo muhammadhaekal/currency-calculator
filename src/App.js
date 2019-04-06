f5ca9689ebdafbe6cee3fa6c42c0831777fc0c0c
Muhammad Haekal <93haekal@gmail.com>
2019-04-06 19:55:12 +0700

    Implement Design From Wireframe

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer>
+        <CalculatorBox>
+          <CurrentValue>
+            <CurrentValDesc>USD - United States Dollar</CurrentValDesc>
+            <CurrentInputContainer>
+              <InputDesc>USD</InputDesc>
+              <CurrValInputForm type="number" />
+            </CurrentInputContainer>
+          </CurrentValue>
+          <ConvertedValues>
+            <ConvertedValDetail>
+              <AmountContainer>
+                <AmmountDetail>IDR</AmmountDetail>
+                <AmmountDetail>14000</AmmountDetail>
+              </AmountContainer>
+              <CurrecyName>IDR - Indonesian Rupiah</CurrecyName>
+              <ConvCurrencyDetail>1 USD = IDR 14,410.00</ConvCurrencyDetail>
+            </ConvertedValDetail>
+          </ConvertedValues>
+        </CalculatorBox>
+      </AppContainer>
     );
   }
 }
+
+const AppContainer = styled.div({
+  display: "flex",
+  justifyContent: "center"
+});
+
+const CalculatorBox = styled.div({
+  width: "600px",
+  border: "2px solid grey"
+});
+
+const CurrentValue = styled.div({
+  padding: "15px 15px 15px 15px",
+  borderBottom: "2px solid grey"
+});
+
+const ConvertedValues = styled.div({
+  margin: "15px 15px 15px 15px"
+});
+
+const CurrentValDesc = styled.h3({
+  margin: "0 0"
+});
+
+const CurrentInputContainer = styled.div({
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+const InputDesc = styled.h2({
+  margin: "0 0"
+});
+
+const CurrValInputForm = styled.input({
+  border: "2px solid grey",
+  borderRadius: "5px",
+  width: "200px",
+  textAlign: "right"
+});
+
+const ConvertedValDetail = styled.div({
+  border: "2px solid grey",
+  borderRadius: "2px",
+  padding: "15px"
+});
+
+const AmountContainer = styled.div({
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+const AmmountDetail = styled.div({
+  fontSize: "28px"
+});
+
+const CurrecyName = styled.div({
+  fontWeight: "bolder",
+  margin: "8px 0"
+});
+
+const ConvCurrencyDetail = styled.div({});
 
 export default App;
