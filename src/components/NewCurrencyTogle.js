e868cadaa09235d8f0cbd8b6ef11d68ae5412813
Muhammad Haekal <93haekal@gmail.com>
2019-04-07 14:31:08 +0700

    Change New Currency Input to Select Option

--- src/components/NewCurrencyTogle.js
+++ src/components/NewCurrencyTogle.js
@@ -12,12 +12,29 @@ const NewCurrencyToggle = ({
     <React.Fragment>
       {isInputActive ? (
         <InputNewCurrencyContainer>
-          <InputNewCurrency
+          {/* <InputNewCurrency
             type="text"
             name="newCurrency"
             value={newCurrency}
             onChange={handleOnChange}
-          />
+          /> */}
+          <InputNewCurrency
+            name="newCurrency"
+            value={newCurrency}
+            onChange={handleOnChange}
+          >
+            <option value="JPY">JPY</option>
+            <option value="SGD">SGD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <option value="USD">USD</option>
+            <option value="CAD">CAD</option>
+            <option value="IDR">IDR</option>
+            <option value="CHF">CHF</option>
+            <option value="INR">INR</option>
+            <option value="MYR">MYR</option>
+            <option value="KRW">KRW</option>
+          </InputNewCurrency>
           <SubmitButton onClick={handleOnClick}>Submit</SubmitButton>
         </InputNewCurrencyContainer>
       ) : (
@@ -33,12 +50,15 @@ const InputNewCurrencyContainer = styled.div({
   display: "flex"
 });
 
-const InputNewCurrency = styled.input({
+const InputNewCurrency = styled.select({
   width: "100%",
   border: "2px solid lightgrey",
   borderRadius: "5px",
   marginRight: "15px",
-  fontSize: "18px"
+  fontSize: "18px",
+  "&:hover": {
+    cursor: "pointer"
+  }
 });
 
 const SubmitButton = styled.button({
